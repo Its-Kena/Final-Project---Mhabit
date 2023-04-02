03349945685253a2af1c3a0a3e45883af26ec453
nlee294 <nlee294@uab.edu>
2023-04-01 21:21:16 -0500

    MovieAdapter.kt made and genrePic added to Movie.kt dataclass

--- app/src/test/java/entities/Movie.kt
+++ app/src/test/java/entities/Movie.kt
@@ -1,13 +1,14 @@
 package entities
 
-class Movie(val title: String,
-    //the following are the properties for each movie
-            val rating: Int,
-            val description: String,
-            val minutes: Int,
-            val seconds: Int,
-            val hours: Int,
-            val review: String,
-            val WatchAgain: Boolean)
+data class Movie(//the following are the properties for each movie
+    val genrePic: Int,
+    val title: String,
+    val rating: Float,
+    val description: String,
+    val minutes: Int,
+    val seconds: Int,
+    val hours: Int,
+    val review: String,
+    val WatchAgain: Boolean)
 
 //this is where you should put the database commands
