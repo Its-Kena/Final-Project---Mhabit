27a1cf84a8e09465a0af7b7759016edb11b996d8
briayysha <browndenyka@yahoo.com>
2023-04-09 18:51:37 -0500

    star edits

--- app/src/main/java/activities/MovieDetailActivity.kt
+++ app/src/main/java/activities/MovieDetailActivity.kt
@@ -113,11 +113,11 @@ class MovieDetailActivity : AppCompatActivity() {
                 val starBar = findViewById<RatingBar>(R.id.mhabitStarRating);
                 starBar.setOnRatingBarChangeListener { ratingBar, fl, b ->
                     when(ratingBar.rating.toInt()){
-                        1 -> starBar.rating = 1.0F;
-                        2 -> starBar.rating = 2.0F;
-                        3 -> starBar.rating = 3.0F;
-                        4 -> starBar.rating = 4.0F;
-                        5 -> starBar.rating = 5.0F;
+                        1 -> starBar.rating = 1F;
+                        2 -> starBar.rating = 2F;
+                        3 -> starBar.rating = 3F;
+                        4 -> starBar.rating = 4F;
+                        5 -> starBar.rating = 5F;
                     }
                     }
 
